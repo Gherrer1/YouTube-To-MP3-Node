fc0d6990cb2c6d7378d7bca0041f91524da5dfa9
Gerardo <gherrer1@binghamton.edu>
2018-03-26 13:13:28 -0400

    pretty much done, just want it hosted somewhere should something happen to this comp

---
+++ main.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+const ytdl = require('ytdl-core');
+// const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
+
+let videoID = 'c4G2sWk5jvM';
+
+let stream = ytdl(videoID, {
+  quality: 'highestaudio'
+});
+
+let start = Date.now();
+ffmpeg(stream)
+  .audioBitrate(128)
+  .save(`${__dirname}/${videoID}.mp3`)
+  .on('progress', p => {
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(`${p.targetSize}kb downloaded`);
+  })
+  .on('end', () => {
+    console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`)
+  });
